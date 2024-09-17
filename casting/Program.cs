1dfd934c49c2535efaf64b9e57fd17081ac44992
Christopher <museman3@gmail.com>
2024-09-17 11:31:21 +0200

    Created casting program

---
+++ casting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//int i = 0;
+//long l = 0;
+
+//Går fra et kortere variabel til et længere, så den kan implicit konvertere
+//l = i + 1;
+//Går fra en længere variabel til en kortere, så vi skal bruge casting først
+//i = (int)l;
+
+//Console.WriteLine("Fra int til long: " + i);
+Console.WriteLine("Fra long til int, ved brug af casting: " + l);
+
+float f = 0;
+double d = 0;
+
+//d = f + 0.5;
+//f = Convert.ToSingle(d+0,5);
+
+//Console.WriteLine("Fra float til double: " + d);
+//Console.WriteLine("Fra double til float: " + f);
