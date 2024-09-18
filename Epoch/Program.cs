aa530d25f3d6eb497fc005a309667c2d80733016
Christopher <museman3@gmail.com>
2024-09-18 13:01:08 +0200

    Created a new program called Epoch

---
+++ Epoch/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Seconds passed since 1970, january 1st. Unix epoch time
+int seconds = 1726531205;
+//Amount of seconds in one year
+int OneYear = 31556926;
+//Amount of seconds in one day
+int OneDay = 86400;
+
+
+int years = seconds / OneYear;
+int rest = seconds % OneYear;
+int days = rest / OneDay;
+
+Console.WriteLine("It's been " + years + " years");
+Console.WriteLine("It's been " + days + " days");
+Console.WriteLine(rest);
