0d87a76c7aa172eb4518ce356b26772f324e2c4d
Christopher <museman3@gmail.com>
2024-09-20 13:06:34 +0200

    Program with a function/method that puts two variables together

---
+++ Addition/Program.cs
@@ -0,0 +1,13 @@
+﻿//første variabel
+int x = 5;
+//anden variabel
+int y = 3;
+
+//Vi laver en metode/funktion som vil have to inputs, i og l
+int plus (int i, int l)
+{
+	//Vi vil gerne have funktionen returnerer de to inputs lagt sammen
+	return i+l;
+}
+
+Console.Write(plus(x,y));
