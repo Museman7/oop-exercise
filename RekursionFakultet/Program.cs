9cb7c837f85e038324665cd6810d7e2578a07b15
Christopher <museman3@gmail.com>
2024-09-25 13:29:42 +0200

    Program that deals with factorial and recursion

---
+++ RekursionFakultet/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//lav en metode som har sin egen metode inde i sig selv for at finde fakultet
+
+
+//int fak (int val1)
+//
+//{
+//	val1 = val1 * 2;
+//	return(val1);
+//}
+//
+//Console.Write(fak(value));
+
+int fak (int x)
+{
+	if (x > 0)
+	{
+		//Vi ganger x med metoden(-x) og looper indtil vi rammer 0
+		x = x*fak(x-1);
+		Console.WriteLine(x);
+		
+		return x;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+Console.Write(fak());
